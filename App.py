291dcea91f51e5625d5b50e1b96a7e71b6d1aba4
Sumit K prajapat <140511392+Skpkush@users.noreply.github.com>
2025-03-21 19:28:44 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2239,7 +2239,7 @@ else:
                         "Company": info['name'],
                         "Sector": info['sector'],
                         "Price": price_str,
-                        "Change":  f'{change_str},',
+                        "Change":  f"<span style='color: {change_color};'>{change_str}</span>",
                         "Market Cap": market_cap_str,
                         "P/E Ratio": pe_str,
                         "Dividend Yield": dividend_str
